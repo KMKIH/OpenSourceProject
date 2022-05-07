04c5138864eb2cdb3c46738d680d0a4cd81ef6d8
KMKIH <khkim9811@naver.com>
2022-05-08 01:34:07 +0900

    2022/05/08
    TMP Import
    Title 생성

---
+++ OpenSourceProject/Assets/Script/TitleSceneController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneController : MonoBehaviour
+{
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+    public void LoadStageSelectScene()
+    {
+        LoadingSceneController.Instance.LoadSceneWithFade("StageSelect");
+    }
+}
